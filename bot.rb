599eb50c64f9f4560b1d6aa92d293a417a7f6de5
Marco Perrando <marco.perrando@gmail.com>
2017-04-20 18:03:28 +0200

    Libreria, Tolto respawn. Notifiche a tutti.

--- bot.rb
+++ bot.rb
@@ -1,6 +1,7 @@
 require 'telegram/bot'
 require 'telegram/bot/botan' 
 require 'timers'
+require_relative 'libbot'
 
 SECS = ARGV[0].to_i || 3
 AUTH_FILE = ARGV[1] || "auth_ids.txt"
@@ -8,41 +9,6 @@ TOKEN = ENV['TELEGRAM_TOKEN'] || '342935347:AAEdo4J_iTubxtXkjduaxmfyqvU-cspicgU'
 puts "Timeout for automatic ON is #{SECS} seconds"
 puts "Authorized ids file: #{AUTH_FILE}"
 puts "Telegram token in use: #{TOKEN}"
-
-class Auth
-  require 'listen'
-
-  def initialize file
-    @file = file
-    Listen.to('.', only: /#{Regexp.quote(file)}/) do |modified|
-      refresh
-    end.start
-    @ids = []
-    refresh
-  end
-
-  def authenticate user
-    if ids.include? user.id
-      Caps::ALL.new
-    else
-      false
-    end
-  end
-
-  def ids
-    @ids
-  end
-
-  def refresh
-    puts "Refreshing ids from #{@file}"
-    if File.exist?(@file)
-      @ids = File.readlines(@file).map(&:to_i)
-    else
-      @ids = []
-    end
-    puts "Authorized ids: #{@ids.inspect}"
-  end
-end
 
 class Caps
   class ALL
@@ -79,18 +45,11 @@ class ShellCommands
 end
 
 class Lights
-  def initialize timeout, cmd
+  def initialize timers, timeout, cmd
     @cmd = cmd
     @timeout = timeout
-    @timers = Timers::Group.new
+    @timers = timers
     @my_mutex = Mutex.new
-    Thread.new do
-      loop do
-        @timers.wait do
-          sleep 1
-        end
-      end
-    end
   end
 
   def turn_on
@@ -123,55 +82,58 @@ end
 
 class EUnath < RuntimeError; end
 @auth = Auth.new AUTH_FILE
-@lights = Lights.new SECS, ShellCommands.new
-
+@timers = TimersInThread.new 1
+@lights = Lights.new @timers.timers, SECS, ShellCommands.new
 Telegram::Bot::Client.run(TOKEN) do |bot|
   #bot.enable_botan!('WjrKZsmFeEjMrEVxXekFVb6d-RoDB1sk')
 
-  begin
-    bot.listen do |message|
-      puts message.inspect
-      caps = @auth.authenticate message.from
-      unless caps
-        bot.api.send_message(chat_id: message.chat.id, text: "Utente non autorizzato")
-        next
-      end
-      unless caps.can? message.text
-        bot.api.send_message(chat_id: message.chat.id, text: "Comando non autorizzato")
-        next
-      end
-      case message.text
-      when /^\/start/
-        kb = [
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Spegni'),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Accendi'),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Status'),
-        ]
-        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Benvenuto!', reply_markup: markup)
-      when /^spegni/i
-        bot.api.send_message(chat_id: message.chat.id, text: "Spengo le luci. Verranno riaccese alle #{(Time.now + SECS.to_i).strftime '%R'}.")
-        @lights.turn_off do
-          bot.api.send_message(chat_id: message.chat.id, text: "Tempo scaduto: ho riacceso le luci")
-        end
-      when /^accendi/i
-        @lights.turn_on
-      when /^status/i
-        @lights.on_in.tap do |s|
-          if s && s > 0
-            bot.api.send_message(chat_id: message.chat.id, text: "Riaccensione luci tra #{s.to_i} secondi")
-          else
-            bot.api.send_message(chat_id: message.chat.id, text: "Nessuna riaccensione programmata")
-          end
-        end
-      else
-        bot.api.send_message(chat_id: message.chat.id, text: "Non ho capito cosa mi chiedi")
-      end
+  @notifier = ChatsNotifier.new bot, "chats.txt"
+
+  bot.listen do |message|
+    puts message.inspect
+    caps = if @auth.authenticate message.from
+             Caps::ALL.new
+           else
+             false
+           end
+    unless caps
+      bot.api.send_message(chat_id: message.chat.id, text: "Utente non autorizzato")
+      next
     end
-  rescue
-    puts $!
-    puts $@
-    puts "Respawning"
-    retry
+    unless caps.can? message.text
+      bot.api.send_message(chat_id: message.chat.id, text: "Comando non autorizzato")
+      next
+    end
+    case message.text
+    when /^\/start/
+      kb = [
+        Telegram::Bot::Types::KeyboardButton.new(text: 'Spegni'),
+        Telegram::Bot::Types::KeyboardButton.new(text: 'Accendi'),
+        Telegram::Bot::Types::KeyboardButton.new(text: 'Status'),
+      ]
+      markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+      bot.api.send_message(chat_id: message.chat.id, text: 'Benvenuto!', reply_markup: markup)
+      @notifier.register_chat message.chat.id
+    when /^\/stop/
+      @notifier.unregister_chat message.chat.id
+      bot.api.send_message(chat_id: message.chat.id, text: 'Bye bye! Per riattivare le notifiche scrivei /start', reply_markup: markup)
+    when /^spegni/i
+      @notifier.send text: "Spengo le luci. Verranno riaccese alle #{(Time.now + SECS.to_i).strftime '%R'}."
+      @lights.turn_off do
+        @notifier.send text: "Tempo scaduto: ho riacceso le luci"
+      end
+    when /^accendi/i
+      @notifier.send text: "Accendo le luci."
+      @lights.turn_on
+    when /^status/i
+      @lights.on_in.tap do |s|
+        if s && s > 0
+          bot.api.send_message(chat_id: message.chat.id, text: "Riaccensione luci tra #{s.to_i} secondi")
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: "Nessuna riaccensione programmata")
+        end
+      end
+    else
+    end
   end
 end
---
+++ libbot.rb
@@ -0,0 +1,96 @@
+class Auth
+  require 'listen'
+
+  def initialize file
+    @file = file
+    Listen.to('.', only: /#{Regexp.quote(file)}/) do |modified|
+      refresh
+    end.start
+    @ids = []
+    refresh
+  end
+
+  def authenticate user
+    if ids.include? user.id
+      true
+    else
+      false
+    end
+  end
+
+  def ids
+    @ids
+  end
+
+  def refresh
+    puts "Refreshing ids from #{@file}"
+    if File.exist?(@file)
+      @ids = File.readlines(@file).map(&:to_i)
+    else
+      @ids = []
+    end
+    puts "Authorized ids: #{@ids.inspect}"
+  end
+end
+
+
+class TimersInThread
+  attr_reader :timers
+
+  def initialize resolution_in_secs
+    @timers = Timers::Group.new
+    Thread.new { timers_loop }
+    @resolution_in_secs = resolution_in_secs
+  end
+
+  def timers_loop
+    loop do
+      @timers.wait do
+        sleep @resolution_in_secs
+      end
+    end
+  rescue
+    puts "#{$!}"
+    puts "#{$@}"
+    retry
+  end
+end
+
+class ChatsNotifier
+  def initialize bot, file
+    @bot = bot
+    @chats = Set.new
+    @file = file
+    refresh
+  end
+
+  def refresh
+    puts "Loading chats from #{@file}"
+    if File.exist?(@file)
+      @chats.merge File.readlines(@file).map(&:to_i)
+    end
+    puts "Chats: #{@chats.inspect}"
+  end
+
+  def save
+    File.open(@file, 'w+') do |f|
+      f.write(@chats.to_a.join("\n"))
+    end
+  end
+
+  def register_chat id
+    @chats << id
+    save
+  end
+
+  def unregister_chat id
+    @chats.delete id
+    save
+  end
+
+  def send opts
+    @chats.each do |id|
+      @bot.api.send_message({chat_id: id}.merge opts)
+    end
+  end
+end
